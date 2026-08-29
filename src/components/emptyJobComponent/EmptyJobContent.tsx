82e8d6627be7544b454872acb39d44e1f3430394
Elahe Habibi <elhe.habibi@gmail.com>
2026-08-29 20:41:42 +0330

    feat: add empty job section

---
+++ src/components/emptyJobComponent/EmptyJobContent.tsx
@@ -0,0 +1,12 @@
+function EmptyJobContent() {
+  return (
+    <section className="flex justify-center items-center bg-muted">
+        <div>
+            <p>NOT FOUND</p>
+        </div>
+      
+    </section>
+  )
+}
+
+export default EmptyJobContent
